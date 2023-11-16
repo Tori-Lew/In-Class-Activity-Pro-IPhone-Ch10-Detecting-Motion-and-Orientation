c87968f5cd53f4060b056e4ce6ef7610cad17499
Tori Lew <55900873+Tori-Lew@users.noreply.github.com>
2023-11-15 16:17:44 -0800

    Initial commit

---
+++ In-Class Activity Pro IPhone Ch10 Detecting Motion and Orientation/In_Class_Activity_Pro_IPhone_Ch10_Detecting_Motion_and_OrientationApp.swift
@@ -0,0 +1,17 @@
+//
+//  In_Class_Activity_Pro_IPhone_Ch10_Detecting_Motion_and_OrientationApp.swift
+//  In-Class Activity Pro IPhone Ch10 Detecting Motion and Orientation
+//
+//  Created by Student Account on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct In_Class_Activity_Pro_IPhone_Ch10_Detecting_Motion_and_OrientationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
